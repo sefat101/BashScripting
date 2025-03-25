5402a267c0366e8f0a9fe9f8060852aa8692d039
sefat101 <142144068+sefat101@users.noreply.github.com>
2025-03-26 03:29:07 +0600

    Update singleSeparateArgs.sh

--- Codes/singleSeparateArgs.sh
+++ Codes/singleSeparateArgs.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#./args.sh "Hello World" Linux Shell
+echo "Using \"\$*\":"
+for arg in "$*"; do
+  echo "$arg"
+done
+
+echo "Using \"\$@\":"
+for arg in "$@"; do
+  echo "$arg"
+done
 
